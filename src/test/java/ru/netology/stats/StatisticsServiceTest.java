f7c8b720d4164c3cbb08d9645fdaa3ca088c8fb3
Yuliya Skripkina <yu.skripkina@gmail.com>
2021-12-23 00:02:25 +0300

    added changed pom & StatisticsServiceTest

--- src/test/java/ru/netology/stats/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology.stats;
 import org.junit.jupiter.api.Test;
 import ru.netology.StatisticsService;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
