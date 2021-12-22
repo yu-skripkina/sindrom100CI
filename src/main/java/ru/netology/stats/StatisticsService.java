e9c096507ea7d1b215faa1d926f0b03cce68fa44
Yuliya Skripkina <yu.skripkina@gmail.com>
2021-12-23 01:29:25 +0300

    updated Test

--- src/main/java/ru/netology/stats/StatisticsService.java
+++ src/main/java/ru/netology/stats/StatisticsService.java
@@ -11,7 +11,7 @@ public class StatisticsService {
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
-            if (current_max < income)
+            if (current_max <= income)
                 current_max = income;
         return current_max;
     }
